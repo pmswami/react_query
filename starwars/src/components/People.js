881b7e9ec60ef7e9e264a3a1e9119eebd8952b67
Swam Fire <usern5949@gmail.com>
2025-10-20 14:57:53 +0530

    Added Planets and People components

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,8 +1,33 @@
 import React from "react";
+import {useQuery} from '@tanstack/react-query'
+import Person from "./Person";
+
+const fetchPeople = async ()=>{
+    const res = await fetch("https://swapi.dev/api/people")
+    return res.json()
+}
+
 const People = () => {
-    return ( 
+    const {data, status} = useQuery({
+    queryKey: ['people'],
+    queryFn: fetchPeople,
+    })
+    console.log(data);
+    
+    return (
         <div>
-            <h1>People</h1>
+            <h1>Planets</h1>
+            {status ==="error" &&
+            <div>Error in fetching data</div>
+            }
+
+            {status ==="pending" &&
+            <div>Loading the data ...</div>
+            }
+
+            {status ==="success" &&
+            <div>{data.results.map(person=><div key={person.name}><Person person={person}/></div>)}</div>
+            }
         </div> 
     );
 }
--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,8 +1,33 @@
 import React from "react";
+import {useQuery} from '@tanstack/react-query'
+import Planet from "./Planet";
+
+const fetchPlanets = async ()=>{
+    const res = await fetch("https://swapi.dev/api/planets")
+    return res.json()
+}
+
 const Planets = () => {
+    const {data, status} = useQuery({
+    queryKey: ['planets'],
+    queryFn: fetchPlanets,
+    })
+    console.log(data);
+    
     return (
         <div>
             <h1>Planets</h1>
+            {status ==="error" &&
+            <div>Error in fetching data</div>
+            }
+
+            {status ==="pending" &&
+            <div>Loading the data ...</div>
+            }
+
+            {status ==="success" &&
+            <div>{data.results.map(planet=><div key={planet.name}><Planet planet={planet}/></div>)}</div>
+            }
         </div> 
     );
 }
